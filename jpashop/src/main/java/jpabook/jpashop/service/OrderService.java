f0578f692ea919f9184d8d05bb7057068bd02825
ddkzzang <ddkzzang@naver.com>
2022-08-31 15:52:19 +0900

    주문 기능 Test 개발

--- jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import jpabook.jpashop.domain.Delivery;
+import jpabook.jpashop.domain.DeliveryStatus;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
@@ -32,6 +33,7 @@ public class OrderService {
 		// 배송정보 생성 
 		Delivery delivery = new Delivery();
 		delivery.setAddress(member.getAddress());
+		delivery.setStatus(DeliveryStatus.READY);
 		
 		// 주문상품 생성
 		OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
