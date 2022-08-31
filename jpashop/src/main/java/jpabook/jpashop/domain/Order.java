8973b31ad815f768e5c48d92c6664a8f21f85fa7
ddkzzang <ddkzzang@naver.com>
2022-08-31 11:35:51 +0900

    주문, 주문상품 Entity 개발

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -63,4 +63,43 @@ public class Order {
 		this.delivery = delivery;
 		delivery.setOrder(this);
 	}
+	
+	// -------- 생성 메서드  ---------
+	// 생성할때 값이 변경될 때 이곳을 먼저 확인하면 됨
+	public static Order createOrder(Member member,Delivery delivery, OrderItem... orderItems) {
+		Order order = new Order();
+		order.setMember(member);
+		order.setDelivery(delivery);
+		
+		for(OrderItem orderItem : orderItems) {
+			order.addOrderitem(orderItem);
+		}	
+		order.setStatus(OrderStatus.ORDER);
+		order.setOrderDate(LocalDateTime.now());
+		return order;
+	}
+	
+	// 주문 취소
+	public void cancel() {
+		if(delivery.getStatus() == DeliveryStatus.COMP) {
+			throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+		}
+		
+		this.setStatus(OrderStatus.CANCEL);
+		for(OrderItem orderItem : orderItems) { // 여러 상품 주문했을 경우를 위해
+			orderItem.cancel();
+		}
+	}
+	
+	// ------ 조회 로직 ------
+	// 전체 주문 가격 조회
+	public int getTotalPrice() {
+		int totalPrice = 0;
+		for(OrderItem orderItem : orderItems) {
+			totalPrice += orderItem.getTotalPrice();
+		}
+		return totalPrice;
+//		return orderItems.stream().mapToInt(OrderItem::getTotalPrice).sum();
+//		위에 객제 재생성부터 return 반환까지 같은 로직을 간단하게 작성한 것 
+	}
 }
