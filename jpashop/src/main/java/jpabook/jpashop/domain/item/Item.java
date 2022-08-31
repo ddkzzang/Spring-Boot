dd7bfaab446ba06ceb07495c127afb473424d921
ddkzzang <ddkzzang@naver.com>
2022-08-31 10:21:58 +0900

    상품 Entity개발 (비즈니스 로직 추가)

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -13,6 +13,7 @@ import javax.persistence.InheritanceType;
 import javax.persistence.ManyToMany;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -32,5 +33,20 @@ public abstract class Item {
 	
 	@ManyToMany(mappedBy = "items")
 	private List<Category> categories = new ArrayList<>();
+	
+	// --------------- 비즈니스 로직 -------------
+	// 재고 수량을 증가하는 로직
+	public void addStock(int quantity) {
+		this.stockQuantity += quantity;
+	}
+	
+	// 재고 수량을 감소하는 로직
+	public void removeStock(int quantity) {
+		int restStock = this.stockQuantity - quantity;
+		if(restStock < 0) {
+			throw new NotEnoughStockException("need more stock");
+		}
+		this.stockQuantity = restStock;
+	}
 	
 }
